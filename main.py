8d3187fb9215511459c126e810a207666b2a00b8
Eder Johannes <Johannes.Eder@voestalpine.com>
2020-12-23 10:16:29 +0100

    Working version of the Flask application that displays quotes on the screen

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+import pandas as pd
+import random
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    quote = []
+    author = []
+    with open('Lebensweisheiten.txt') as f:
+        for line in f.readlines():
+            if line != '\n':
+                sentence = line.strip()
+                if sentence.startswith('„'):
+                    quote.append(sentence)
+                else:
+                    author.append(sentence)
+    dictionary = dict(zip(author,quote))
+    random_author = random.choice(author)
+    quote_ = dictionary[random_author]
+    return(render_template("index.html",  author=random_author, quote=quote_))
+
+if __name__ == "__main__":
+    app.run(debug = True)
